e28251c2efbf70a1db367beb705d09902d16fbad
Jesús Gabriel Figueroa Rodríguez <jgabriell_00@hotmail.com>
2022-11-26 17:21:37 -0700

    add more fields in character class

--- lib/dominio/character.dart
+++ lib/dominio/character.dart
@@ -1,23 +1,53 @@
+import 'package:harry_popotes/dominio/problemas.dart';
+
 class Character {
-  final String home;
+  final String house;
   final String name;
+  final String genero;
+  final String dateOfBirth;
+  final String yearOfBirth;
   final bool esEstudiante;
+  final bool esMago;
+  final String patronus;
+  final bool hogwartsStaff;
   final String actor;
+  final bool vivo;
+  final String specie;
   final String imagen;
 
   Character(
-      {required this.home,
+      {required this.genero,
+      required this.esMago,
+      required this.patronus,
+      required this.hogwartsStaff,
+      required this.vivo,
+      required this.specie,
+      required this.dateOfBirth,
+      required this.yearOfBirth,
+      required this.house,
       required this.name,
       required this.esEstudiante,
       required this.actor,
       required this.imagen});
 
-  factory Character.obtenerInfoDeJson(Map<String, dynamic> json) {
-    return Character(
-        home: json['home'],
-        name: json['name'],
-        esEstudiante: json['hogwartsStudent'],
-        actor: json['actor'],
-        imagen: json['imagen']);
+  factory Character.constructor({required Map<String, dynamic> json}) {
+    try {
+      return Character(
+          house: json['house'],
+          name: json['name'],
+          specie: json['species'],
+          genero: json['gender'],
+          dateOfBirth: json['dateOfBirth'],
+          yearOfBirth: json['yearOfBirth'].toString(),
+          esMago: json['wizard'],
+          patronus: json['patronus'],
+          esEstudiante: json['hogwartsStudent'],
+          hogwartsStaff: json['hogwartsStaff'],
+          actor: json['actor'],
+          vivo: json['alive'],
+          imagen: json['image']);
+    } catch (e) {
+      throw FormatoJsonPersonajeErroneo();
+    }
   }
 }
