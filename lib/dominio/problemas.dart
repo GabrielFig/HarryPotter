a06b110d6933d33d489a6499028603933e60b0e0
Jesús Gabriel Figueroa Rodríguez <jgabriell_00@hotmail.com>
2022-11-26 17:21:52 -0700

    add problems file

---
+++ lib/dominio/problemas.dart
@@ -0,0 +1,9 @@
+abstract class Problema {}
+
+class PersonajeNoExiste extends Problema {}
+
+class FormatoJsonPersonajeErroneo extends Problema {}
+
+class DireccionErronea extends Problema {}
+
+class JuegoJugadoMalFormado extends Problema {}
