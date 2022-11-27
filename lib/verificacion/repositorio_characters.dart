c24999e374b4a3d3866f1f0d97584624127cb0bf
Jesús Gabriel Figueroa Rodríguez <jgabriell_00@hotmail.com>
2022-11-26 17:22:21 -0700

    add repository local and online

--- lib/verificacion/repositorio_characters.dart
+++ lib/verificacion/repositorio_characters.dart
@@ -1,21 +1,58 @@
 import 'dart:convert';
+import 'dart:io';
+import 'package:fpdart/fpdart.dart';
 
 import 'package:harry_popotes/dominio/character.dart';
+import 'package:harry_popotes/dominio/problemas.dart';
 import "package:http/http.dart" as http;
 
 abstract class RepositorioCharacters {
-  Future<List<Character>> juntarPersonajes() async {
-    List<Character> list = List<Character>();
-    http.Response response =
-        await http.get(Uri.parse("http://hp-api.herokuapp.com/api/characters"));
+  Future<Either<Problema, List<Character>>> obtenerPersonajes(String direccion);
+}
 
+class RepositorioCharactersOnline extends RepositorioCharacters {
+  @override
+  Future<Either<Problema, List<Character>>> obtenerPersonajes(
+      String direccion) async {
+    final response = await http.get(Uri.parse(direccion));
     if (response.statusCode == 200) {
       var json = jsonDecode(response.body);
-      list = json.map<Character>((item) => Character.obtenerInfoDeJson(item)).toList();
-      list.sort((a, b) => a.name.compareTo(b.name));
-      return list;
-    } else {
-      return null;
+      final resultado = _obtenerListaPersonajes(json);
+      resultado.match((l) {
+        return Left(l);
+      }, (r) {
+        return Right(r);
+      });
     }
+    return Left(DireccionErronea());
+
+    //list.sort((a, b) => a.name.compareTo(b.name));
+  }
+}
+
+class RepositorioCharactersOffline extends RepositorioCharacters {
+  @override
+  Future<Either<Problema, List<Character>>> obtenerPersonajes(
+      String direccion) async {
+    List<dynamic> file = jsonDecode(File(direccion).readAsStringSync());
+
+    final resultado = _obtenerListaPersonajes(file);
+    resultado.match((l) {
+      return Left(l);
+    }, (r) {
+      return Right(r);
+    });
+    throw UnimplementedError();
+  }
+}
+
+Either<Problema, List<Character>> _obtenerListaPersonajes(List<dynamic> lista) {
+  try {
+    List<Character> personajes = lista
+        .map<Character>((item) => Character.constructor(json: item))
+        .toList();
+    return Right(personajes);
+  } catch (e) {
+    return Left(FormatoJsonPersonajeErroneo());
   }
 }
