e12fecf72305548f0082097373b8ff6249bcf74f
Jesús Gabriel Figueroa Rodríguez <jgabriell_00@hotmail.com>
2022-11-28 23:07:20 -0700

    add test to spell repository

--- test/verificacion/repositorio_characters_test.dart
+++ test/verificacion/repositorio_characters_test.dart
@@ -1,3 +1,29 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:harry_popotes/dominio/character.dart';
+import 'package:harry_popotes/dominio/problemas.dart';
+import 'package:harry_popotes/verificacion/repositorio_characters.dart';
 
-void main() {}
+void main() {
+  test('se espera 4 personajes y es una lista de personajes :D', () async {
+    RepositorioCharactersOffline repoOffline = RepositorioCharactersOffline();
+    var resultado = await repoOffline
+        .obtenerPersonajes('./test/verificacion/lista_personajes/lista1.json');
+    resultado.match((l) {
+      assert(false);
+    }, (r) {
+      expect(r.length, 4);
+      expect(r, isA<List<Character>>());
+    });
+  });
+
+  test('da error con un json mal formado', () async {
+    RepositorioCharactersOffline repoOffline = RepositorioCharactersOffline();
+    var resultado = await repoOffline
+        .obtenerPersonajes('./test/verificacion/lista_personajes/lista2.json');
+    resultado.match((l) {
+      expect(l, isA<FormatoJsonPersonajeErroneo>());
+    }, (r) {
+      assert(true);
+    });
+  });
+}
