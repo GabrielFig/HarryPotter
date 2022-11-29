1c37c063c7d779701d8c1fedbe81f00487b5ad11
Jesús Gabriel Figueroa Rodríguez <jgabriell_00@hotmail.com>
2022-11-28 23:07:44 -0700

    add try catch lines to return a problem in left

--- lib/verificacion/repositorio_characters.dart
+++ lib/verificacion/repositorio_characters.dart
@@ -8,6 +8,8 @@ import "package:http/http.dart" as http;
 
 abstract class RepositorioCharacters {
   Future<Either<Problema, List<Character>>> obtenerPersonajes(String direccion);
+  Future<Either<Problema, List<Character>>> obtenerPersonajesPorCasa(
+      String direccion, nombreCasa);
 }
 
 class RepositorioCharactersOnline extends RepositorioCharacters {
@@ -18,7 +20,7 @@ class RepositorioCharactersOnline extends RepositorioCharacters {
     if (response.statusCode == 200) {
       var json = jsonDecode(response.body);
       final resultado = _obtenerListaPersonajes(json);
-      resultado.match((l) {
+      return resultado.match((l) {
         return Left(l);
       }, (r) {
         return Right(r);
@@ -28,21 +30,59 @@ class RepositorioCharactersOnline extends RepositorioCharacters {
 
     //list.sort((a, b) => a.name.compareTo(b.name));
   }
+
+  @override
+  Future<Either<Problema, List<Character>>> obtenerPersonajesPorCasa(
+      String direccion, nombreCasa) async {
+    String rutaCompleta = direccion + nombreCasa;
+    final response = await http.get(Uri.parse(rutaCompleta));
+    if (response.statusCode == 200) {
+      var json = jsonDecode(response.body);
+      final resultado = _obtenerListaPersonajes(json);
+      return resultado.match((l) {
+        return Left(l);
+      }, (r) {
+        return Right(r);
+      });
+    }
+    return Left(DireccionErronea());
+  }
 }
 
 class RepositorioCharactersOffline extends RepositorioCharacters {
   @override
   Future<Either<Problema, List<Character>>> obtenerPersonajes(
       String direccion) async {
-    List<dynamic> file = jsonDecode(File(direccion).readAsStringSync());
+    try {
+      List<dynamic> file = jsonDecode(File(direccion).readAsStringSync());
 
+      final resultado = _obtenerListaPersonajes(file);
+      return resultado.match((l) {
+        return Left(l);
+      }, (r) {
+        return Right(r);
+      });
+    } catch (e) {
+      return Left(FormatoJsonPersonajeErroneo());
+    }
+  }
+
+  @override
+  Future<Either<Problema, List<Character>>> obtenerPersonajesPorCasa(
+      String direccion, nombreCasa) async {
+    String direccionCompleta = '$direccion/$nombreCasa.json';
+    List<dynamic> file = [];
+    try {
+      file = jsonDecode(File(direccionCompleta).readAsStringSync());
+    } catch (e) {
+      return Left(FormatoJsonPersonajeErroneo());
+    }
     final resultado = _obtenerListaPersonajes(file);
-    resultado.match((l) {
+    return resultado.match((l) {
       return Left(l);
     }, (r) {
       return Right(r);
     });
-    throw UnimplementedError();
   }
 }
 
