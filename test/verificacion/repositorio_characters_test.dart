c1994d0c61433f62b1126fe5f58567da305d9224
GabrielFig <jgabriell_00@hotmail.com>
2022-11-29 12:03:39 -0700

    add test online

--- test/verificacion/repositorio_characters_test.dart
+++ test/verificacion/repositorio_characters_test.dart
@@ -4,26 +4,52 @@ import 'package:harry_popotes/dominio/problemas.dart';
 import 'package:harry_popotes/verificacion/repositorio_characters.dart';
 
 void main() {
-  test('se espera 4 personajes y es una lista de personajes :D', () async {
-    RepositorioCharactersOffline repoOffline = RepositorioCharactersOffline();
-    var resultado = await repoOffline
-        .obtenerPersonajes('./test/verificacion/lista_personajes/lista1.json');
-    resultado.match((l) {
-      assert(false);
-    }, (r) {
-      expect(r.length, 4);
-      expect(r, isA<List<Character>>());
+  group('Pruebas Offline', () {
+    test('se espera 4 personajes y es una lista de personajes :D', () async {
+      RepositorioCharactersOffline repoOffline = RepositorioCharactersOffline();
+      var resultado = await repoOffline.obtenerPersonajes(
+          './test/verificacion/lista_personajes/lista1.json');
+      resultado.match((l) {
+        assert(false);
+      }, (r) {
+        expect(r.length, 4);
+        expect(r, isA<List<Character>>());
+      });
+    });
+
+    test('da error con un json mal formado', () async {
+      RepositorioCharactersOffline repoOffline = RepositorioCharactersOffline();
+      var resultado = await repoOffline.obtenerPersonajes(
+          './test/verificacion/lista_personajes/lista2.json');
+      resultado.match((l) {
+        expect(l, isA<FormatoJsonPersonajeErroneo>());
+      }, (r) {
+        assert(true);
+      });
     });
   });
 
-  test('da error con un json mal formado', () async {
-    RepositorioCharactersOffline repoOffline = RepositorioCharactersOffline();
-    var resultado = await repoOffline
-        .obtenerPersonajes('./test/verificacion/lista_personajes/lista2.json');
-    resultado.match((l) {
-      expect(l, isA<FormatoJsonPersonajeErroneo>());
-    }, (r) {
-      assert(true);
+  group('Pruebas Online', () {
+    test('Se espera una lista de personajes', () async {
+      RepositorioCharactersOnline repoOnline = RepositorioCharactersOnline();
+      var resultado = await repoOnline
+          .obtenerPersonajes('https://hp-api.herokuapp.com/api/characters');
+      resultado.match((l) {
+        assert(false);
+      }, (r) {
+        expect(r, isA<List<Character>>());
+      });
+    });
+
+    test('Se espera una lista de personajes', () async {
+      RepositorioCharactersOnline repoOnline = RepositorioCharactersOnline();
+      var resultado = await repoOnline
+          .obtenerPersonajes('https://hp-api.herokuapp.com/api/characters');
+      resultado.match((l) {
+        assert(false);
+      }, (r) {
+        expect(r.length, 403);
+      });
     });
   });
 }
