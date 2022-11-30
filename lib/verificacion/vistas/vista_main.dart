5209a1a08afb89f0a980b1ad2384a543ca29ed9b
Jesús Gabriel Figueroa Rodríguez <jgabriell_00@hotmail.com>
2022-11-29 22:11:33 -0700

    create main view

---
+++ lib/verificacion/vistas/vista_main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class VistaMainPage extends StatelessWidget {
+  const VistaMainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
